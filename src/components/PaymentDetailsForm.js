f0f6e0a28c39ff64bf1a46b6b609810fd6aaae74
mattc0llier <matt.collier@hotmail.com>
2018-11-06 16:47:41 +0000

    added empty event handlers

--- src/components/PaymentDetailsForm.js
+++ src/components/PaymentDetailsForm.js
@@ -4,10 +4,10 @@ const PaymentDetailsForm = () => {
   return (
     <div className="payment-details-form">
       <h3>Set up payment</h3>
-      <form className="payment-details-form__form">
-        <input type="text" placeholder="Card number"/>
-        <input type="text" placeholder="Expiry date"/>
-        <input type="text" placeholder="CCV"/>
+      <form className="payment-details-form__form" onSubmit={}>
+        <input type="text" placeholder="Card number" onChange={}/>
+        <input type="text" placeholder="Expiry date" onChange={}/>
+        <input type="text" placeholder="CCV" onChange={}/>
         <p>Set up a donor account?</p>
         <p>Continue donating anonymously:</p>
         <button>Donate -Price- to -First Name-</button>
