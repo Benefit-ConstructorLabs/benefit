4a0e7c9d86f1f6f0b223eaee1f431740db05e5eb
Roland Levy <rolandjlevy@gmail.com>
2018-11-06 17:33:54 +0000

    Re-ordered QR code comp and Recipient comp

--- src/components/Recipient.js
+++ src/components/Recipient.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import QRCode from './QRCode'
+import QRCode from './QRCode';
+import '../../styles/components/recipient.scss';
 
 const Recipient = () => {
   return (
+
     <React.Fragment>
+      
       <h2>Recipient Component</h2>
-      <QRCode />
+
+        <h2>You Unique QR code</h2>
+
+        <QRCode />
+
+        <h3 className='recipient__steps'>3 easy steps to receive donations</h3>
+        <ul className='recipient__steps-list'>
+          <li>
+            Display your unique code to potential donors
+          </li>
+          <li>
+            Donors scan your QR code and set a payment
+          </li>
+          <li>
+            Check your total donations
+          </li>
+        </ul>
+
+        <button className='recipient__print-qrcode'>
+          Print your QR code
+        </button>
+
+        <button className='recipient__view-donations'>
+          See your donation total
+        </button>
+
     </React.Fragment>
   )
 };
