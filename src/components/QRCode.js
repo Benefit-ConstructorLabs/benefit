8413f688aabf20fb7eb87c26ca3f59c1ee924e5f
Roland Levy <rolandjlevy@gmail.com>
2018-11-06 16:30:35 +0000

    Added QR code component

--- src/components/QRCode.js
+++ src/components/QRCode.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import QRCodePackage from 'qrcode';
 
-const QRCode = () => {
-  return (
-    <p>QR code component</p>
-  )
-};
+class QRCode extends React.Component {
+
+  renderQRCode() {
+    const url = 'http://www.benefit.com/recipient/3'; // url will eventually come from container -> this.props.url
+    QRCodePackage.toCanvas(this.canvas, url, { 
+      errorCorrectionLevel: 'M',
+      maskPattern: 4,
+      scale: 4,
+      version: 5, 
+        color: {
+        dark: '#3333ffff',
+        light: '#ffff9966'
+      }
+    },
+    error => {
+      if (error) {
+        console.error(error);
+      }
+      console.log('success!');
+    })
+  }
+
+  componentDidMount() {
+    this.renderQRCode();
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <p>QR code component</p>
+        <canvas ref={element => {this.canvas = element}} />
+      </React.Fragment>
+    );
+  }
+}
 
 export default QRCode;
