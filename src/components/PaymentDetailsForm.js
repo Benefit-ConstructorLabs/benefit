1ee565fc9c34157ae24d33e6e15a088c5040dc75
mattc0llier <matt.collier@hotmail.com>
2018-11-06 16:44:51 +0000

    basic payment details html set up

--- src/components/PaymentDetailsForm.js
+++ src/components/PaymentDetailsForm.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 const PaymentDetailsForm = () => {
   return (
-    <p>Payment Details Form</p>
+    <div className="payment-details-form">
+      <h3>Set up payment</h3>
+      <form className="payment-details-form__form">
+        <input type="text" placeholder="Card number"/>
+        <input type="text" placeholder="Expiry date"/>
+        <input type="text" placeholder="CCV"/>
+        <p>Set up a donor account?</p>
+        <p>Continue donating anonymously:</p>
+        <button>Donate -Price- to -First Name-</button>
+      </form>
+    </div>
   )
 };
 
