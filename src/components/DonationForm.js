136eb0044136ac8e4b9b750c452b48e994784ee1
Joe Lamb <joe@elevenplustwo.co.uk>
2018-11-06 17:01:55 +0000

    DonationForm and DonationHeader components and associated stylesheets

--- src/components/DonationForm.js
+++ src/components/DonationForm.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import '../../styles/components/donation-form.scss';
 
 const DonationForm = () => {
   return (
-    <p>Donation form</p>
+    <React.Fragment>
+      <img src="https://via.placeholder.com/150?text=Recipient+photo" alt="Recipient photo" className="donation__recipient__photo" />
+      <h3 className="donation__purpose">Donate towards a night in a shelter</h3>
+      <form onSubmit={event => event.preventDefault()} className="donation__form">
+        <p>
+          <label htmlFor="donation__amount" className="donation__amount__label">
+            How much?
+            <input type="number" placeholder="£1" className="donation__amount__input" />
+            <input type="range" />
+          </label>
+        </p>
+        <p className="donation__quick-amount">
+          <label htmlFor="low">
+            <input type="radio" name="quick-amount" id="low" value="50p" />
+            50p
+          </label>
+          <label htmlFor="mid">
+            <input type="radio" name="quick-amount" id="mid" value="£2" />
+            £2
+          </label>
+          <label htmlFor="high">
+            <input type="radio" name="quick-amount" id="high" value="£5" />
+            £5
+          </label>
+        </p>
+        <button type="submit">Donate to firstname</button>
+      </form>
+    </React.Fragment >
   )
 };
 
 export default DonationForm;
+
