671460b1d40ff519b97ea0c26733f94bf2dcbdaf
Joe Lamb <joe@elevenplustwo.co.uk>
2018-11-06 23:30:56 +0000

    eslint config and update components

--- src/components/PaymentAcknowledge.js
+++ src/components/PaymentAcknowledge.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import '../../styles/components/payment-acknowledge.scss'
+import '../../styles/components/payment-acknowledge.scss';
 
-const PaymentAcknowledge = () => {
-  return (
-    <section className='acknowledgement' >
-      <p className='acknowledgement__notification'>Donation confirmed</p>
-      <h2 className='acknowledgement__confirmation'>All set. Thanks for helping "FirstName" out!</h2>
-      <h3 className='acknowledgement__question'>Want to save your details for another donation?</h3>
-      <button className='btn acknowledgement__account__btn' onClick={event=>event.preventDefault()}>Set up donor account</button>
-    </section>
-  )
-};
+const PaymentAcknowledge = () => (
+  <section className="acknowledgement">
+    <p className="acknowledgement__notification">Donation confirmed</p>
+    <h2 className="acknowledgement__confirmation">All set. Thanks for helping &ldquo; FirstName&rdquo; out!</h2>
+    <h3 className="acknowledgement__question">Want to save your details for another donation?</h3>
+    <button type="button" className="btn acknowledgement__account__btn" onClick={event => event.preventDefault()}>Set up donor account</button>
+  </section>
+);
 
 export default PaymentAcknowledge;
--- src/components/QRCode.js
+++ src/components/QRCode.js
@@ -2,36 +2,35 @@ import React from 'react';
 import QRCodePackage from 'qrcode';
 
 class QRCode extends React.Component {
-
   componentDidMount() {
     this.renderQRCode();
   }
 
   renderQRCode() {
     // url will come from container -> this.props.url
-    const url = 'http://www.benefit.com/recipient/3'; 
-    QRCodePackage.toCanvas(this.canvas, url, { 
+    const url = 'http://www.benefit.com/recipient/3';
+    QRCodePackage.toCanvas(this.canvas, url, {
       errorCorrectionLevel: 'M',
       maskPattern: 4,
       scale: 4,
-      version: 5, 
-        color: {
+      version: 5,
+      color: {
         dark: '#00737EFF',
-        light: '#CDF8FFCC'
-      }
+        light: '#CDF8FFCC',
+      },
     },
-    error => {
-      if (error) {
-        console.error(error);
-      }
-      console.log('success!');
-    })
+      (error) => {
+        if (error) {
+          // console.error(error);
+        }
+        // console.log('success!');
+      });
   }
 
   render() {
     return (
       <React.Fragment>
-        <canvas ref={element => {this.canvas = element}} />
+        <canvas ref={(element) => { this.canvas = element; }} />
       </React.Fragment>
     );
   }
