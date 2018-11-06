e12698ea0aedb66469cd96315486c414225adb08
YelChristensen <yelena.christensen@gmail.com>
2018-11-06 16:47:46 +0000

    PaymentAcknowledge component

--- src/components/PaymentAcknowledge.js
+++ src/components/PaymentAcknowledge.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const PaymentAcknowledge = () => {
   return (
-    <React.Fragment>
-      <p>Donation confirmed</p>
-      <h2>All set. Thanks for helping "FirstName" out!</h2>
-      <h3>Want to save your details for another donation?</h3>
-      <button onClick={event=>event.preventDefault()}>Set up donor account</button>
-    </React.Fragment>
+    <section className='acknowledgement' >
+      <p className='acknowledgement__notification'>Donation confirmed</p>
+      <h2 className='acknowledgement__confirmation'>All set. Thanks for helping "FirstName" out!</h2>
+      <h3 className='acknowledgement__question'>Want to save your details for another donation?</h3>
+      <button className='acknowledgement__account' onClick={event=>event.preventDefault()}>Set up donor account</button>
+    </section>
   )
 };
 
