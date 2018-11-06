89727162065949832debd7c92de57a0cf72af72b
Joe Lamb <joe@elevenplustwo.co.uk>
2018-11-06 17:22:15 +0000

    RecipientProfile and stylesheet

--- src/components/DonationForm.js
+++ src/components/DonationForm.js
@@ -7,26 +7,17 @@ const DonationForm = () => {
       <img src="https://via.placeholder.com/150?text=Recipient+photo" alt="Recipient photo" className="donation__recipient__photo" />
       <h3 className="donation__purpose">Donate towards a night in a shelter</h3>
       <form onSubmit={event => event.preventDefault()} className="donation__form">
+        <h4>How much?</h4>
         <p>
-          <label htmlFor="donation__amount" className="donation__amount__label">
-            How much?
-            <input type="number" placeholder="£1" className="donation__amount__input" />
-            <input type="range" />
-          </label>
+          <input type="number" placeholder="£1" className="donation__amount__input" id="donation__amount" />
+        </p>
+        <p>
+          <input type="range" />
         </p>
         <p className="donation__quick-amount">
-          <label htmlFor="low">
-            <input type="radio" name="quick-amount" id="low" value="50p" />
-            50p
-          </label>
-          <label htmlFor="mid">
-            <input type="radio" name="quick-amount" id="mid" value="£2" />
-            £2
-          </label>
-          <label htmlFor="high">
-            <input type="radio" name="quick-amount" id="high" value="£5" />
-            £5
-          </label>
+          <button>50p</button>
+          <button>£1</button>
+          <button>£2</button>
         </p>
         <button type="submit">Donate to firstname</button>
       </form>
--- src/components/RecipientProfile.js
+++ src/components/RecipientProfile.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import '../../styles/components/recipient-profile.scss';
 
 const RecipientProfile = () => {
   return (
-    <p>Recipient profile</p>
+    <React.Fragment>
+      <h3 className="recipient-profile__headline">Fancy a chat?</h3>
+      <p className="recipient-profile__intro">firstName is interested in:</p>
+      <ol className="recipient__interests">
+        <li className="recipient__interest">First interest</li>
+        <li className="recipient__interest">Second interest</li>
+        <li className="recipient__interest">Third interest</li>
+      </ol>
+    </React.Fragment>
   )
 };
 
