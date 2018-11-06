6f6506ab176151c920384cb7cba825814c601b90
YelChristensen <yelena.christensen@gmail.com>
2018-11-06 17:37:43 +0000

    basic framework in scss file

--- src/components/PaymentAcknowledge.js
+++ src/components/PaymentAcknowledge.js
@@ -7,7 +7,7 @@ const PaymentAcknowledge = () => {
       <p className='acknowledgement__notification'>Donation confirmed</p>
       <h2 className='acknowledgement__confirmation'>All set. Thanks for helping "FirstName" out!</h2>
       <h3 className='acknowledgement__question'>Want to save your details for another donation?</h3>
-      <button className='acknowledgement__account' onClick={event=>event.preventDefault()}>Set up donor account</button>
+      <button className='btn acknowledgement__account__btn' onClick={event=>event.preventDefault()}>Set up donor account</button>
     </section>
   )
 };
