5bc778563aac4dc51da35f3c8f8efa8a002ef18b
YelChristensen <yelena.christensen@gmail.com>
2018-11-06 16:42:11 +0000

    basic PaymentAcknowledge component

--- src/components/PaymentAcknowledge.js
+++ src/components/PaymentAcknowledge.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const PaymentAcknowledge = () => {
   return (
-    <p>Payment acknowledgement</p>
+    <React.Fragment>
+      <p>Donation confirmed</p>
+      <h2>All set. Thanks for helping "FirstName" out!</h2>
+      <h3>Want to save your details for another donation?</h3>
+      <button onClick={event=>event.preventDefault()}>Set up donor account</button>
+    </React.Fragment>
   )
 };
 
