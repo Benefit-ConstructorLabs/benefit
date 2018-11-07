97ad47fad116ea9da99093135c43457be5c7b757
nicola gibson <nicola.k.gibson@gmail.com>
2018-11-07 15:53:51 +0000

    updated newrecipient with password and username, container shell

--- src/components/NewRecipient.js
+++ src/components/NewRecipient.js
@@ -5,27 +5,25 @@ import '../../styles/components/newrecipient.scss';
 const NewRecipient = () => (
   <React.Fragment>
     <h2>New Recipient</h2>
-    <h2 className="newrecipient__title">
-      Start taking digital donations in 3 steps
-    </h2>
+    <h2 className="newrecipient__title">Start taking digital donations in 3 steps</h2>
     <img src="" alt="" height="" width="" />
     <form>
-      <label className="newrecipient__intro">
-        Add your basic information
-        </label>
-      <input type="text" text="firstname" placeholder="First name" />
-      <input type="text" text="lastname" placeholder="Last name" />
-      <input type="text" text="telephone" placeholder="Telephone number" />
-      <button className="newrecipient__upload-button">
+      <h2 className="newrecipient__intro">Add your basic information</h2>
+      <input type="text" text="firstname" id="firstname" placeholder="First name" />
+      <input type="text" text="lastname" id="lastname" placeholder="Last name" />
+      <input type="text" text="telephone" id="telephone" placeholder="Telephone number" />
+      <input type="text" text="username" id="username" placeholder="Choose your username" />
+      <input type="text" text="password" id="password" placeholder="Set a password" />
+      <button className="newrecipient__photo-button" type="submit" value="Submit">
         Upload a picture
-        </button>
-      <label>Now tell people 3 things about you...</label>
-      <input type="text" text="text" placeholder="1. I play the piano" />
-      <input type="text" text="text" placeholder="2. I like white coffee" />
-      <input type="text" text="text" placeholder="3. I'm a Rugby fan" />
-      <button className="newrecipient__qr-button">
+      </button>
+      <h2>Now tell people 3 things about you...</h2>
+      <input type="text" text="text" id="textbox1" placeholder="1. I play the piano" />
+      <input type="text" text="text" id="textbox2" placeholder="2. I like white coffee" />
+      <input type="text" text="text" id="textbox3" placeholder="3. I'm a Rugby fan" />
+      <button className="newrecipient__qr-button" type="submit" value="Submit">
         Generate Your Unique QR Code
-        </button>
+      </button>
     </form>
 
     <RecipientSignUpForm />
---
+++ src/containers/NewRecipientContainer.js
@@ -0,0 +1,18 @@
+// import { connect } from 'react-redux';
+// import NewRecipient from '../components/NewRecipient';
+
+// const mapStateToProps = reduxState => {
+//   return {
+
+//   };
+// }
+
+// const mapDispatchToProps = dispatch => {
+//     return {
+
+//     }
+// }
+
+// const NewRecipientContainer = connect()(NewRecipient);
+
+// export default NewRecipientContainer;
