c9bfd1e00b1ff6da756080b5375f414bb77c78df
YelChristensen <yelena.christensen@gmail.com>
2018-11-07 12:56:18 +0000

    changed quotation marks

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const pgp = require("pg-promise")();
+const bodyParser = require('body-parser');
+const pgp = require('pg-promise')();
 const db = pgp({
-  host: "localhost",
+  host: 'localhost',
   port: 5432,
   database: process.env.DB_NAME,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD
 });
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 app.use(bodyParser.json());
-app.use("/static", express.static("static"));
-app.set("view engine", "hbs");
-app.set("port", process.env.PORT || 8080);
+app.use('/static', express.static('static'));
+app.set('view engine', 'hbs');
+app.set('port', process.env.PORT || 8080);
 
-app.get("/api/recipients", (req, res) => {
+app.get('/api/recipients', (req, res) => {
   db.any(`SELECT * FROM recipient`)
     .then(data => res.json(data))
     .catch(error => res.json({ error: error.message }));
 });
 
 //adds a new recipient to the database
-app.post("/api/recipient", (req, res) => {
+app.post('/api/recipient', (req, res) => {
   const { recipient } = req.body;
   bcrypt
     .hash(recipient.password, saltRounds)
     .then(function(hash) {
       return db.one(
-        "INSERT INTO recipient (first_name, last_name, tel, photo, username, password, qrcode) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id",
+        'INSERT INTO recipient (first_name, last_name, tel, photo, username, password, qrcode) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id',
         [
           recipient.first_name,
           recipient.last_name,
@@ -46,7 +46,7 @@ app.post("/api/recipient", (req, res) => {
     })
     .then(result => {
       return db.one(
-        "INSERT INTO biography (recipient_id, bio_1, bio_2, bio_3) VALUES ($1, $2, $3, $4)RETURNING recipient_id",
+        'INSERT INTO biography (recipient_id, bio_1, bio_2, bio_3) VALUES ($1, $2, $3, $4)RETURNING recipient_id',
         [result.id, recipient.bio_1, recipient.bio_2, recipient.bio_3]
       );
     })
@@ -55,10 +55,10 @@ app.post("/api/recipient", (req, res) => {
 });
 
 //add donation to the database
-app.post("/api/donation", (req, res) => {
+app.post('/api/donation', (req, res) => {
   const { donation } = req.body;
   return db.one(
-      "INSERT INTO donation (recipient_id, donor_id, amount, stripe_id) VALUES ($1, $2, $3, $4) RETURNING id",
+      'INSERT INTO donation (recipient_id, donor_id, amount, stripe_id) VALUES ($1, $2, $3, $4) RETURNING id',
       [
         donation.recipient_id,
         donation.donor_id,
@@ -84,7 +84,7 @@ const sendSMS = (first_name, tel) => {
   const accountSid = process.env.TWILIO_ACCOUNT_SID;
   const authToken = process.env.TWILIO_AUTH_TOKEN;
   const fromNumber = process.env.TWILIO_FROM_NUMBER;
-  const client = require("twilio")(accountSid, authToken);
+  const client = require('twilio')(accountSid, authToken);
   const number = tel;
   client.messages
     .create({
@@ -96,10 +96,10 @@ const sendSMS = (first_name, tel) => {
     .done();
 };
 
-app.use("/", (req, res) => {
-  res.render("index");
+app.use('/', (req, res) => {
+  res.render('index');
 });
 
-app.listen(app.get("port"), () => {
-  console.log(`Listening on ${app.get("port")}`);
+app.listen(app.get('port'), () => {
+  console.log(`Listening on ${app.get('port')}`);
 });
