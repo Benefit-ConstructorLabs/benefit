2396c90c4f0cff46a67b9131df94d4becbcd833a
YelChristensen <yelena.christensen@gmail.com>
2018-11-08 18:57:10 +0000

    additional get functions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
 const pgp = require('pg-promise')();
@@ -31,7 +32,7 @@ app.set('port', process.env.PORT || 8080);
 AWS.config.update({
   region: process.env.S3_REGION,
   accessKeyId: process.env.S3_ACCESS_KEY_ID,
-  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
 });
 
 // configure AWS to work with promises
@@ -44,7 +45,7 @@ const uploadFile = (buffer, name, type) => {
     Body: buffer,
     Bucket: process.env.S3_BUCKET,
     ContentType: type.mime,
-    Key: `${name}.${type.ext}`
+    Key: `${name}.${type.ext}`,
   };
   const s3 = new AWS.S3();
   return s3.upload(params).promise();
@@ -53,49 +54,44 @@ const uploadFile = (buffer, name, type) => {
 // POST route for S3 uploads
 app.post('/api/upload', (request, response) => {
   const form = new multiparty.Form();
-    form.parse(request, async (error, fields, files) => {
-      try {
-        if (error) throw new Error(error);
-        const path = files.file[0].path;
-        const buffer = fs.readFileSync(path);
-        const type = fileType(buffer);
-        const timestamp = Date.now().toString();
-        const fileName = `${timestamp}-lg`;
-        const data = await uploadFile(buffer, fileName, type);
-        return response.status(200).send(data);
-      } catch (error) {
-        return response.status(400).send(error);
-      }
-    });
-});
-
-app.get('/api/recipient/:id', (req, res) => { 
-  const id = req.params.id
-  db.any(`SELECT * FROM recipient WHERE id=$1`, [id])
-    .then(data => res.json(data))
-    .catch(error => res.json({ error: error.message }));  
+  form.parse(request, async (error, fields, files) => {
+    try {
+      if (error) throw new Error(error);
+      const path = files.file[0].path;
+      const buffer = fs.readFileSync(path);
+      const type = fileType(buffer);
+      const timestamp = Date.now().toString();
+      const fileName = `${timestamp}-lg`;
+      const data = await uploadFile(buffer, fileName, type);
+      return response.status(200).send(data);
+    } catch (error) {
+      return response.status(400).send(error);
+    }
+  });
 });
 
 app.get('/api/recipient', (req, res) => {
   db.any('SELECT * FROM recipient')
     .then(data => res.json(data))
-    .catch(error => res.json({ error: error.message }));  
+    .catch(error => res.json({ error: error.message }));
 });
 
 // retrieve recipient by id
 app.get('/api/recipient/:id', (req, res) => {
   const { id } = req.params;
-  return db.one('SELECT id, first_name, photo FROM recipient WHERE id=$1', [id]).then(data => db
-    .one('SELECT * FROM biography WHERE id = $1', [data.id])
+  return db
+    .one('SELECT id, first_name, photo FROM recipient WHERE id=$1', [id])
+    .then(data => db
+      .one('SELECT * FROM biography WHERE id = $1', [data.id])
     /* eslint-disable camelcase */
-    .then(({ bio_1, bio_2, bio_3 }) => {
-      res.json(Object.assign({}, data, { bio: [bio_1, bio_2, bio_3] }));
-    }))
+      .then(({ bio_1, bio_2, bio_3 }) => {
+        res.json(Object.assign({}, data, { bio: [bio_1, bio_2, bio_3] }));
+      }))
     .catch(error => res.json({ error: error.message }));
   /* eslint-enable camelcase */
 });
 
-// adds a new recipient to the database
+// add new recipient to the database
 app.post('/api/recipient', (req, res) => {
   const { recipient } = req.body;
   bcrypt
@@ -156,6 +152,28 @@ app.post('/api/donation', (req, res) => {
       // sendSMS(donation.recipient_name, donation.tel);
       return res.json(json);
     })
+    .catch(error => res.json({ error: error.message }));
+});
+
+// add new donor to the database
+app.post('/api/donor', (req, res) => {
+  const { donor } = req.body;
+  bcrypt
+    .hash(donor.password, saltRounds)
+    .then(hash => db.one(
+      'INSERT INTO donor (first_name, last_name, email, password, tel, stripe) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id',
+      [donor.first_name, donor.last_name, donor.email, hash, donor.tel, donor.stripe],
+    ))
+    .then(result => res.json(result))
+    .catch(error => res.json({ error: error.message }));
+});
+
+// retrieve all donations for recipient
+app.get('/api/donations/:id', (req, res) => {
+  const { id } = req.params;
+  return db
+    .any('SELECT SUM(amount) FROM donation WHERE recipient_id=$1', [id])
+    .then(amounts => res.json(amounts))
     .catch(error => res.json({ error: error.message }));
 });
 
