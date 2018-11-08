d2846658f39a95892cadead7f99303f45237c173
Matt Collier <matt.collier@hotmail.com>
2018-11-08 15:07:11 +0000

    comments deleted

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import App from './components/App';
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, /*composeEnhancers(*/applyMiddleware(
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(
   thunkMiddleware,
-))//);
+)));
 
 ReactDOM.render(
   <Provider store={store}>
