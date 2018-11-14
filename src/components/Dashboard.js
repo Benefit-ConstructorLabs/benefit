eeecb0ae5378c0bf30561379f7157f080b1f68ea
Joe Lamb <joe@elevenplustwo.co.uk>
2018-11-14 23:05:43 +0000

    dashboard with live data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,7 +6,7 @@ import {
   Cell, XAxis, YAxis, Tooltip, Legend,
   CartesianGrid, Area, Bar, Line,
 } from 'recharts';
-import { format, getTime } from 'date-fns';
+import { format, getTime, isToday } from 'date-fns';
 import '../../styles/components/dashboard.scss';
 
 class Dashboard extends React.Component {
@@ -19,16 +19,21 @@ class Dashboard extends React.Component {
     const { donations } = this.props;
 
     let total = 0;
-    let todayTotal = 0;
+
+    console.log('TODAY', donations.filter(item => isToday(item.time)));
+
+    const todaysTotal = (donations.filter(item => isToday(item.time)).reduce(((acc, item) => acc + item.amount), 0) / 100).toFixed(2);
+
+    // console.log('TODAY', todaysDonations);
 
     const timelineData = [...donations];
     timelineData.reverse();
 
-    const timeSeries = timelineData.map((donation, index) => {
-      const { time, amount } = donation;
-      total += (amount / 10);
+    const timeSeries = timelineData.map((item, index) => {
+      const { time, amount } = item;
+      total += (amount / 100);
       return {
-        time: getTime(time), amount: amount / 10, total, average: total / (index + 1),
+        time: getTime(time), donation: amount / 100, total, average: total / (index + 1),
       };
     });
 
@@ -37,55 +42,70 @@ class Dashboard extends React.Component {
       { name: 'Donors', today: 300, other: 1308 },
     ];
 
-    // pie chart
-    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#BADA55'];
-    const donationData = [
-      { name: '£2', value: 400 },
-      { name: '£5', value: 300 },
-      { name: '£10', value: 100 },
-      { name: '£1', value: 200 },
-      { name: '£20', value: 40 },
-    ];
-
     // radar
-    const dailyData = [
-      { subject: 'Monday', A: 120, B: 110, fullMark: 150 },
-      { subject: 'Tuesday', A: 98, B: 130, fullMark: 150 },
-      { subject: 'Wednesday', A: 86, B: 130, fullMark: 150 },
-      { subject: 'Thursday', A: 99, B: 100, fullMark: 150 },
-      { subject: 'Friday', A: 85, B: 90, fullMark: 150 },
-      { subject: 'Saturday', A: 65, B: 85, fullMark: 150 },
-    ];
+    console.log('DONATIONS', donations);
+
+    const dailySeries = donations.map((item) => {
+      const { time, amount } = item;
+      return { day: format(time, 'dddd'), amount: amount / 100 };
+    });
+
+    console.log('DAILY SERIES', dailySeries);
+
+    const days = [
+      { day: 'Monday', amount: 0 },
+      { day: 'Tuesday', amount: 0 },
+      { day: 'Wednesday', amount: 0 },
+      { day: 'Thursday', amount: 0 },
+      { day: 'Friday', amount: 0 },
+      { day: 'Saturday', amount: 0 },
+      { day: 'Sunday', amount: 0 }];
+
+    dailySeries.forEach((item) => {
+      if (days.find(element => element.day === item.day)) {
+        days.find(element => element.day === item.day).amount += item.amount;
+      } else {
+        days.push(item);
+      }
+    });
+
+    console.log('DAILY TOTALS', days);
 
     return (
 
-      <section className="dashboard">
-        <h2 className="dashboard__title">Dashboard</h2>
+      <section className="dashboard" >
+        <h2 className="dashboard__title">Your Dashboard</h2>
         <dl className="dashboard__totals__overall">
-          <dd>Total</dd>
+          <dd>Total Donations</dd>
           <dt>{`£${total}`}</dt>
         </dl>
         <dl className="dashboard__totals__daily">
           <dd>Today’s total</dd>
-          <dt>£564</dt>
+          <dt>{`£${todaysTotal}`}</dt>
         </dl>
         <section className="dashboard__progress">
-          <ComposedChart width={960} height={400} data={timeSeries}
-            margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+          <h3>Donations timeline</h3>
+          <ComposedChart
+            width={960}
+            height={400}
+            data={timeSeries}
+            margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+          >
             <CartesianGrid stroke="#f5f5f5" />
-            <XAxis dataKey="time" type="number" domain={['auto', 'auto']} tickFormatter={time => format(time, 'D/M/YYYY')} />
-            <YAxis yAxisId="left" orientation="left" stroke="#BADA55" label={{ value: 'total (£)', angle: -90, position: 'insideLeft' }} />
-            <YAxis yAxisId="right" orientation="right" stroke="#8884d8" label={{ value: 'donation (£)', angle: -90, position: 'insideRight' }} />
-            <Tooltip />
+            <XAxis dataKey="time" type="number" stroke="#aaa" domain={['dataMin', 'dataMax']} tickFormatter={time => format(time, 'D MMM')} />
+            <YAxis yAxisId="left" orientation="left" stroke="#aaa" label={{ value: 'total (£)', angle: -90, position: 'left' }} />
+            <YAxis yAxisId="right" orientation="right" stroke="#aaa" label={{ value: 'donation (£)', angle: -90, position: 'right' }} />
+            <Tooltip labelFormatter={label => format(label, 'D/M/YYYY')} formatter={value => `£${value.toFixed(2)}`} />
             <Legend />
-            <Area yAxisId="left" type='monotone' dataKey='total' fill="#BADA55" stroke="#BADA55" />
-            <Bar yAxisId="right" dataKey='amount' barSize={40} fill='#413ea0' />
-            <Line yAxisId="right" type='monotone' dataKey='average' stroke="#ff7300" />
+            <Area yAxisId="left" type="monotone" dataKey="total" fill="#ccc" stroke="#aaa" />
+            <Bar yAxisId="right" dataKey="donation" fill="#555" />
+            <Line yAxisId="right" type="monotone" dataKey="average" stroke="#888" />
           </ComposedChart>
         </section>
 
         <section className="dashboard__signups">
-          <BarChart width={300} height={300} data={signupData}
+          <h3>Sign-ups by type</h3>
+          <BarChart width={300} height={400} data={signupData}
             margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
           >
             <CartesianGrid strokeDasharray="3 3" />
@@ -93,46 +113,25 @@ class Dashboard extends React.Component {
             <YAxis />
             <Tooltip />
             <Legend />
-            <Bar dataKey="other" stackId="a" fill="#8884d8" />
-            <Bar dataKey="today" stackId="a" fill="#82ca9d" />
+            <Bar dataKey="other" stackId="a" fill="#ccc" />
+            <Bar dataKey="today" stackId="a" fill="#333" />
           </BarChart>
         </section>
 
-        <section className="dashboard__amounts">
-          <PieChart width={400} height={300}>
-            <Pie
-              data={donationData}
-              dataKey="value"
-              nameKey="name"
-              // cx={120}
-              cy={130}
-              startAngle={180}
-              endAngle={0}
-              innerRadius={80}
-              outerRadius={120}
-              fill="#8884d8"
-              paddingAngle={5}
-            >
-              {
-                donationData.map((entry, index) => <Cell key={index} fill={COLORS[index % COLORS.length]} />)
-              }
-            </Pie>
-            <Legend verticalAlign="middle" />
-          </PieChart>
-        </section>
         <section className="dashboard__daily">
-          <RadarChart outerRadius="90%" width={300} height={300} data={dailyData}>
+          <h3>Donations by day of the week</h3>
+          <RadarChart outerRadius="80%" width={600} height={400} data={days}>
             <PolarGrid />
-            <PolarAngleAxis dataKey="subject" />
-            <PolarRadiusAxis />
-            <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+            <PolarAngleAxis dataKey="day" />
+            <Tooltip formatter={value => `£${value.toFixed()}`} />
+            <Radar name="Donations" dataKey="amount" stroke="#bbb" fill="#ddd" fillOpacity={0.6} />
           </RadarChart>
         </section>
         <section className="dashboard__donations">
+          <h3>Donations data</h3>
           <table className="dashboard__donations__table">
             <thead>
               <tr>
-                <th>ID</th>
                 <th>Date</th>
                 <th>Amount</th>
                 <th>Recipient</th>
@@ -144,9 +143,8 @@ class Dashboard extends React.Component {
                 const { id, time, amount, recipient, donor } = donation;
                 return (
                   <tr key={id}>
-                    <td>{id}</td>
                     <td>{format(time, 'D/M/YYYY')}</td>
-                    <td>{`£ ${Number.parseFloat(amount / 10).toFixed(2)}`}</td>
+                    <td>{`£ ${Number.parseFloat(amount / 100).toFixed(2)}`}</td>
                     <td>{recipient}</td>
                     <td>{donor}</td>
                   </tr>
