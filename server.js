d59c8078301d5f5591dde0e26b7173d83c1e9595
Joe Lamb <joe@elevenplustwo.co.uk>
2018-11-14 12:03:50 +0000

    import donations data

--- server.js
+++ server.js
@@ -323,6 +323,21 @@ app.get('/api/donations/donor/:id', (req, res) => {
     .catch(error => res.json({ error: error.message }));
 });
 
+// retrieve all donations by organisation id
+app.get('/api/donations/organisation/:id', (req, res) => {
+  const { id } = req.params;
+  return db
+    .any(`SELECT donation.id, donation.time_stamp as time, donation.amount,
+          CONCAT(recipient.first_name,' ',recipient.last_name) as recipient,
+          CONCAT(donor.first_name,' ',donor.last_name) as donor
+          FROM donation, recipient, donor
+          WHERE recipient.id = donation.recipient_id
+          AND donor.id = donation.donor_id
+          AND recipient.organisation_id = $1`, [id])
+    .then(donations => res.json(donations))
+    .catch(error => res.json({ error: error.message }));
+});
+
 app.use('/', (req, res) => {
   res.render('index');
 });
--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import NewDonationContainer from '../containers/NewDonationContainer';
 import NewDonorContainer from '../containers/NewDonorContainer';
 import LoginContainer from '../containers/LoginContainer';
 import DonorContainer from '../containers/DonorContainer';
-import Dashboard from './Dashboard';
+import DashboardContainer from '../containers/DashboardContainer';
 import AppFooter from './AppFooter';
 
 import '../../styles/components/app.scss';
@@ -23,7 +23,7 @@ const App = () => (
       <Route path="/recipient/:id/donation" component={NewDonationContainer} />
       <Route exact path="/donor" component={NewDonorContainer} />
       <PrivateRouteContainer exact path="/donor/:id" component={DonorContainer} />
-      <Route exact path="/dashboard" component={Dashboard} />
+      <Route exact path="/dashboard" component={DashboardContainer} />
       <AppFooter />
     </React.Fragment>
   </Router>
