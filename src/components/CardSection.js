34289032fe30d1589234c20a196158633106350a
mattc0llier <matt.collier@hotmail.com>
2018-11-17 19:09:44 +0000

    Added input styling to stripe card element

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,13 +1,21 @@
 // CardSection.js
 import React from 'react';
 import { CardElement } from 'react-stripe-elements';
+import '../../styles/components/card-section.scss'
 
 class CardSection extends React.Component {
   render() {
+
     return (
       <label>
         Card details
-        <CardElement style={{base: {fontSize: '18px'}}} />
+        <ul className=".input" >
+          <li>
+            <div className="input">
+              <CardElement style={{base: {fontSize: '20px'}}} />
+            </div>
+          </li>
+        </ul>
       </label>
     );
   }
