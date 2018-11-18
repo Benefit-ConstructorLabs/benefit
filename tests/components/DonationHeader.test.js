a07bc4e44b6d3215959a258b6ad81ef251c3d941
Joe Lamb <joe@elevenplustwo.co.uk>
2018-11-18 17:23:03 +0000

    snapshot tests updated

--- tests/components/DonationHeader.test.js
+++ tests/components/DonationHeader.test.js
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer';
 import DonationHeader from '../../src/components/DonationHeader';
 
 describe('DonationHeader', () => {
-  test.skip('should render correctly', () => {
+  test('should render correctly', () => {
     const tree = renderer
       .create(<DonationHeader firstName="Bill" />)
       .toJSON();
