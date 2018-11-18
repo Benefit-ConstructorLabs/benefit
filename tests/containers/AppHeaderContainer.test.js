927fb86115b1ca7a0db340fbc8cec96df15f73b8
Matt Collier <matt.collier@hotmail.com>
2018-11-18 18:16:09 +0000

    Update AppHeaderContainer.test.js

--- tests/containers/AppHeaderContainer.test.js
+++ tests/containers/AppHeaderContainer.test.js
@@ -1,7 +1,7 @@
 import { mapStateToProps } from '../../src/containers/AppHeaderContainer';
 
 describe('App Header Container', () => {
-  test.skip('mapStateToProps extracts isLoggedIn, name, userID & userType from state', () => {
+  test('mapStateToProps extracts isLoggedIn, name, userID & userType from state', () => {
     const mockState = {
       login: {
         isLoggedIn: true,
