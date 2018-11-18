1d4c16458b9362ad29404b6e2a46e011a069664e
Matt Collier <matt.collier@hotmail.com>
2018-11-18 18:15:11 +0000

    Update PaymentDetailsForm.test.js

--- tests/components/PaymentDetailsForm.test.js
+++ tests/components/PaymentDetailsForm.test.js
@@ -6,7 +6,7 @@ import PaymentDetailsForm from '../../src/components/PaymentDetailsForm';
 // Please be sure the component that calls createSource or createToken is withinan <Elements> component.
 
 describe('DonationForm', () => {
-  test.skip('should render correctly', () => {
+  test('should render correctly', () => {
     const tree = renderer
       .create(<PaymentDetailsForm />)
       .toJSON();
