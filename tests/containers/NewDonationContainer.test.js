188f697a019ffa41946e098d84c657dbaf411893
Matt Collier <matt.collier@hotmail.com>
2018-11-18 18:19:31 +0000

    Update NewDonationContainer.test.js

--- tests/containers/NewDonationContainer.test.js
+++ tests/containers/NewDonationContainer.test.js
@@ -1,7 +1,7 @@
 import { mapStateToProps } from '../../src/containers/NewDonationContainer';
 
 describe('NewDonationContainer', () => {
-  test.skip('mapStateToProps extracts donation amount from state', () => {
+  test('mapStateToProps extracts donation amount from state', () => {
     const mockState = { donation: { donationAmount: 5 }, view: { showPaymentDetails: false }, recipient: { firstName: 'Bill', photo: '', bio: [] } };
     const expectedOutput = {
       donationAmount: 5,
