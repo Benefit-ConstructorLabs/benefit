02f93400fe29c6389dbf672b9b57041ff1d5035d
Matt Collier <matt.collier@hotmail.com>
2018-11-18 18:15:59 +0000

    Update PrivateRoute.test.js

--- tests/components/PrivateRoute.test.js
+++ tests/components/PrivateRoute.test.js
@@ -4,7 +4,7 @@ import PrivateRoute from '../../src/components/PrivateRoute';
 
 // Invariant Violation: You should not use <Route> or withRouter() outside a <Router>
 describe(PrivateRoute, () => {
-  test.skip('matches the snapshot', () => {
+  test('matches the snapshot', () => {
     const tree = renderer.create(<PrivateRoute />).toJSON();
     expect(tree).toMatchSnapshot();
   });
