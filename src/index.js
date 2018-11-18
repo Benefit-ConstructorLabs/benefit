1177eea8f6c092b9fe36ba9cdc75bc7c0a953674
mattc0llier <matt.collier@hotmail.com>
2018-11-18 12:09:21 +0000

    moved stripe provider store to just cover stripe payment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import thunkMiddleware from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import { StripeProvider } from 'react-stripe-elements';
 import rootReducer from './reducers';
 // import { checkLogin } from './actions';
 import AppWrapperContainer from './containers/AppWrapperContainer';
@@ -16,9 +15,7 @@ const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMid
 
 ReactDOM.render(
   <Provider store={store}>
-    <StripeProvider apiKey="pk_test_fy9Zps4yuxSNHtPrVr5vr02d">
       <AppWrapperContainer />
-    </StripeProvider>
   </Provider>,
   document.getElementById('root'),
 );
