78af4162d5489bc43ac793126d49f96b22c18be1
Matt Collier <matt.collier@hotmail.com>
2018-11-18 18:13:59 +0000

    Update NewDonor.test.js

--- tests/components/NewDonor.test.js
+++ tests/components/NewDonor.test.js
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer';
 import NewDonor from '../../src/components/NewDonor';
 
 describe(NewDonor, () => {
-  test.skip('should render correctly', () => {
+  test('should render correctly', () => {
     const tree = renderer.create(<NewDonor />).toJSON();
     expect(tree).toMatchSnapshot();
   });
